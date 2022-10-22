36e96d786441ae8b3b58bdac69524b8faab9298a
league <league@imac-21.attlocal.net>
2022-10-22 12:56:55 -0700

    Pizza,RobotInSpace,TheRiddler

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,7 +1,26 @@
-
+PImage pepperoni;
+PImage mushroom;
+PImage olive;
 void setup() {
-    
+ size(1000,1000);
+ fill(#FFBC00);
+ ellipse(500,500,900,900);
+ fill(#FF0009);
+ ellipse(500,500,850,850);
+ fill(#FFEE29);
+ ellipse(500,500,800,800);
+ pepperoni = loadImage("pepperoni.png");
+ mushroom = loadImage("mushroom.png");
+ olive = loadImage("olive.png");
 }
 void draw() {
-    
+    if (mousePressed && (mouseButton == LEFT)) {
+ image(pepperoni,mouseX,mouseY);
+ }
+ else if (mousePressed && (mouseButton == RIGHT)) {
+ image(olive,mouseX,mouseY);
+ }
+ else if(mousePressed) {
+ mushroom = loadImage("mushroom.png");
+ }
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -2,24 +2,39 @@ package _04_int._1_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score = 0;
+String good = "Correct!";
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String input = JOptionPane.showInputDialog(null, "What has to be broken before you use it? (Just write one word)" );
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if(input.equals("egg")) {
+	JOptionPane.showMessageDialog(null, good);
+	score =+10000;
+} else {
+	 JOptionPane.showMessageDialog(null, "Wrong!" );
+	score =-10000;
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+	
 		// 6. Add some more riddles
-
+input = JOptionPane.showInputDialog(null, "I am an odd number. Take away a letter and I become even. What number am I?" );
+if(input.equals("seven")) {
+	JOptionPane.showMessageDialog(null, good);
+	score =+10000;
+} else {
+	 JOptionPane.showMessageDialog(null, "Wrong!" );
+	score =-10000;
+}
 		// 2. Make a pop up to show the score.
-		
-	}
+	JOptionPane.showMessageDialog(null, "Your score is" + score);
+	
+}
 }
 
