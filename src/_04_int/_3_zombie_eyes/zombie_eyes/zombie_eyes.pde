951d99a2ec4a08045a60edc48b68c2dcea821f2a
league <league@imac-21.attlocal.net>
2022-10-29 12:57:08 -0700

    Badgers(IP),MyAges,rocketShip,zombieEyes

--- src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,7 +1,16 @@
-
+PImage face;
 void setup() {
-
+size(500,500);
+   face = loadImage("face.jpeg");
+   face.resize(500,500);
+   background(face);
 }
 void draw() {
+  fill(255,mouseX,mouseY);
+  ellipse(185,240,75,115);
+  ellipse(100,275,75,100);
+  fill(3,3,3);
+  ellipse(100,275,20,35);
+  ellipse(185,240,20,35);
 
 }
---
+++ src/_05_for_loops/_2_badgers/Badgers.java
@@ -0,0 +1,17 @@
+package _05_for_loops._2_badgers;
+
+import javax.swing.JOptionPane;
+
+public class Badgers {
+public static void main(String[] args) {
+int badger = 0;
+int snake = 0;
+int mushroom = 0;
+	for(badger=0;badger<13;badger++) {
+		System.out.print(" badger");
+	}
+	for(mushroom=0;mushroom<3;mushroom++) {
+		System.out.println(" snake");
+	}
+}
+}
